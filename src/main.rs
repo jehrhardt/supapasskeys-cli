07969dbb13cb835e7d97598f67d2852086eb6e4a
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-02-07 10:04:42 +0100

    Add start subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "supapasskeys", about = "Supapasskeys extension for Supabase", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(about = "Start local Supapasskeys instance and connect to Supabase", long_about = None)]
+    Start,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Start => {
+            println!("Start command");
+        }
+    }
 }
